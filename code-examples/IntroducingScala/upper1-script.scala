58b2f7d6631485468e6f66cbaae05aebf041a5f4
Ferry Wang <foomango@gmail.com>
2015-07-27 20:31:52 +0800

    Download source code from http://examples.oreilly.com/9780596155964/programming-scala-1st-edition-code-examples.tgz

--- code-examples/IntroducingScala/upper1-script.scala
+++ code-examples/IntroducingScala/upper1-script.scala
@@ -1,3 +1,5 @@
+// code-examples/IntroducingScala/upper1-script.scala
+
 class Upper {
   def upper(strings: String*): Seq[String] = {
     strings.map((s:String) => s.toUpperCase())
--- code-examples/IntroducingScala/upper2-script.scala
+++ code-examples/IntroducingScala/upper2-script.scala
@@ -1,5 +1,7 @@
+// code-examples/IntroducingScala/upper2-script.scala
+
 object Upper {
   def upper(strings: String*) = strings.map(_.toUpperCase())
 }
 
-println("A", "First", "Scala", "Program")
+println(Upper.upper("A", "First", "Scala", "Program"))
