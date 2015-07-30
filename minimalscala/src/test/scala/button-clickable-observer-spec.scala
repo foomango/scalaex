8518c7f28c080aafa7ecd3fc5350ea58efefcaf6
Ferry Wang <foomango@gmail.com>
2015-07-30 19:32:23 +0800

    Add button-clickable-observer-spec.scala

---
+++ minimalscala/src/test/scala/button-clickable-observer-spec.scala
@@ -0,0 +1,18 @@
+// code-examples/Traits/ui2/button-clickable-observer-spec.scala
+
+package ui2
+import org.scalatest._
+import observer._
+import ui.ButtonCountObserver
+
+object ButtonClickableObserverSpec extends FlatSpec with Matchers {
+  "A Button Observer" should 
+    "observe button clicks" in {
+      val observableButton = new Button("Okay") with ObservableClicks
+      val buttonClickCountObserver = new ButtonCountObserver
+      observableButton.addObserver(buttonClickCountObserver)
+
+      for (i <- 1 to 3) observableButton.click()
+      buttonClickCountObserver.count should equal(3)
+    }
+}
