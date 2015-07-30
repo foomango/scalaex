af4f1c109c74a5c02a4a67d4a8e351471a110786
Ferry Wang <foomango@gmail.com>
2015-07-30 19:54:41 +0800

    button-clickable-observer-vetoable-spec.scala

---
+++ minimalscala/src/test/scala/button-clickable-observer-vetoable-spec.scala
@@ -0,0 +1,19 @@
+// code-examples/Traits/ui2/button-clickable-observer-vetoable-spec.scala
+
+package ui2
+import org.scalatest._
+import observer._
+import ui.ButtonCountObserver
+
+object ButtonClickableObserverVetoableSpec extends FlatSpec with Matchers {
+  "A Button Observer with Vetoable Clicks" should 
+    "observe only the first button click" in {
+      val observableButton = 
+          new Button("Okay") with ObservableClicks with VetoableClicks
+      val buttonClickCountObserver = new ButtonCountObserver
+      observableButton.addObserver(buttonClickCountObserver)
+
+      for (i <- 1 to 3) observableButton.click()
+      buttonClickCountObserver.count should equal(1)
+    }
+}
