e641920796b8958033d57e5cb57bdc5b18fb8685
Ferry Wang <foomango@gmail.com>
2015-08-09 12:38:02 +0800

    Add call stack

--- code-examples/ObjectSystem/linearization/linearization2-script.scala
+++ code-examples/ObjectSystem/linearization/linearization2-script.scala
@@ -3,22 +3,27 @@
 var clist = List[String]()
 
 class C1 {
+  println("In C1: ")
   clist ::= "C1"
 }
 
 trait T1 extends C1 {
+  println("In T1: ")
   clist ::= "T1"
 }
 
 trait T2 extends C1 {
+  println("In T2: ")
   clist ::= "T2"
 }
 
 trait T3 extends C1 {
+  println("In T3: ")
   clist ::= "T3"
 }
 
 class C2 extends T1 with T2 with T3 {
+  println("In C2: ")
   clist ::= "C2"
 }
 
