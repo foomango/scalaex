a90c89db5e8c43ff6e3eeefdd790b9ce4ae41c94
Ferry Wang <foomango@gmail.com>
2015-08-09 12:33:18 +0800

    Print call stack

--- code-examples/ObjectSystem/linearization/linearization1-script.scala
+++ code-examples/ObjectSystem/linearization/linearization1-script.scala
@@ -1,23 +1,22 @@
-// code-examples/ObjectSystem/linearization/linearization1-script.scala
-
+// code-examples/ObjectSystem/linearization/linearization1-script.scala 
 class C1 {
   def m = List("C1")
 }
 
 trait T1 extends C1 {
-  override def m = { "T1" :: super.m }
+  override def m = {println("In T1: "); "T1" :: super.m }
 }
 
 trait T2 extends C1 {
-  override def m = { "T2" :: super.m }
+  override def m = { println("In T2: "); "T2" :: super.m }
 }
 
 trait T3 extends C1 {
-  override def m = { "T3" :: super.m }
-}
+  override def m = { println("In T3; "); "T3" :: super.m } 
+} 
 
 class C2 extends T1 with T2 with T3 {
-  override def m = { "C2" :: super.m }
+  override def m = { println("In C2: "); "C2" :: super.m }
 }
 
 val c2 = new C2
